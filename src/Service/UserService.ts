e53858080d1aa77ac54d8e1789af955e615bef49
陳庭浩 <112939670+Howard3897613@users.noreply.github.com>
2025-01-02 17:49:45 +0800

    service 重寫。resp.body還是有問題

--- src/Service/UserService.ts
+++ src/Service/UserService.ts
@@ -9,55 +9,43 @@ import { resp } from "../utils/resp";
 
 export class UserService extends Service {
 
-    public async getAllDatas(): Promise<Array<DBResp<Account>> | undefined> {
-        try {
-            const res: Array<DBResp<Account>> = await accountModel.find({});
-            return res;
-        } catch (error) {
-            return undefined;
-        }
-    }
-
     /**
      * 新增帳戶
      * @param info 帳戶資訊
      * @returns resp
      */
     public async insertOne(info: Account): Promise<resp<DBResp<Account> | undefined>> {
-
-        const current = await this.getAllDatas();
         const resp: resp<DBResp<Account> | undefined> = {
             code: 200,
             message: "",
             body: undefined
         };
 
-        if (current && current.length > 0) {
-            try {
-                if (current.length >= 200) {
-                    resp.message = "account list is full";
-                    resp.code = 403;
-                } else {
-                    info.id = current.length + 1;
-                    const res = new accountModel(info);
-                    resp.body = await res.save();
-                    resp.message = "account added successfully";
-                }
-            } catch (error) {
-                resp.message = "server error";
-                resp.code = 500;
+        try {
+            const existingAccount = await accountModel.findOne({ id: info.id });
+            if (existingAccount) {
+                resp.code = 403;
+                resp.message = "Account already exists";
+            } else {
+                const newAccount = new accountModel(info);
+                resp.body = await newAccount.save();
+                resp.message = "Account created successfully";
             }
-        } else {
-            resp.message = "server error";
+        } catch (error) {
             resp.code = 500;
+            resp.message = `Server error: ${(error as Error).message}`;
         }
 
         return resp;
     }
 
-    public async deleteById(id: number) {
-
-        const resp: resp<any> = {
+    /**
+     * 刪除帳戶
+     * @param id 帳戶ID
+     * @returns resp
+     */
+    public async deleteById(id: number): Promise<resp<DBResp<Account> | undefined>> {
+        const resp: resp<DBResp<Account> | undefined> = {
             code: 200,
             message: "",
             body: undefined
@@ -65,39 +53,71 @@ export class UserService extends Service {
 
         try {
             const res = await accountModel.deleteOne({ id: id });
-            resp.message = "success";
-            resp.body = res;
+            if (res.deletedCount === 0) {
+                resp.code = 404;
+                resp.message = "Account not found";
+            } else {
+                resp.message = "Account deleted successfully";
+                resp.body = res;
+            }
         } catch (error) {
-            resp.message = error as string;
             resp.code = 500;
+            resp.message = `Server error: ${(error as Error).message}`;
         }
 
         return resp;
     }
 
-    public async updateBalanceById(id: number, balance: number) {
+    /**
+     * 查詢所有帳戶
+     * @returns resp
+     */
+    public async getAllAccounts(): Promise<resp<Array<DBResp<Account>> | undefined>> {
+        const resp: resp<Array<DBResp<Account>> | undefined> = {
+            code: 200,
+            message: "",
+            body: undefined
+        };
 
+        try {
+            const res: Array<DBResp<Account>> = await accountModel.find({});
+            resp.body = res;
+            resp.message = "Accounts retrieved successfully";
+        } catch (error) {
+            resp.code = 500;
+            resp.message = `Server error: ${(error as Error).message}`;
+        }
+
+        return resp;
+    }
+
+    /**
+     * 更新帳戶餘額
+     * @param id 帳戶ID
+     * @param balance 新餘額
+     * @returns resp
+     */
+    public async updateBalanceById(id: number, balance: number): Promise<resp<DBResp<Account> | undefined>> {
         const resp: resp<DBResp<Account> | undefined> = {
             code: 200,
             message: "",
-            body: undefined,
+            body: undefined
         };
 
-        const account = await accountModel.findOne({ id: id });
-
-        if (account) {
-            try {
+        try {
+            const account = await accountModel.findOne({ id: id });
+            if (account) {
                 account.balance = balance;
                 await account.save();
                 resp.body = account;
-                resp.message = "update success";
-            } catch (error) {
-                resp.code = 500;
-                resp.message = "server error";
+                resp.message = "Balance updated successfully";
+            } else {
+                resp.code = 404;
+                resp.message = "Account not found";
             }
-        } else {
-            resp.code = 404;
-            resp.message = "account not found";
+        } catch (error) {
+            resp.code = 500;
+            resp.message = `Server error: ${(error as Error).message}`;
         }
 
         return resp;
